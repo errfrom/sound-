c303abb2a3106b2ec0767d0016b02d97d2927d7b
Dmitri Schuiski <errfrom@yandex.ru>
2019-11-26 17:57:38 +0300

    Pointers Manager & DeviceInfo.

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,72 @@
 {-# LANGUAGE QuasiQuotes                        #-}
 {-# LANGUAGE TemplateHaskell                    #-}
+{-# LANGUAGE ExtendedDefaultRules               #-}
 {-# LANGUAGE DeriveAnyClass, DeriveDataTypeable #-}
-{-# LANGUAGE DataKinds, KindSignatures          #-}
+{-# LANGUAGE ConstraintKinds                    #-}
+{-# LANGUAGE ExistentialQuantification          #-}
+{-# LANGUAGE ScopedTypeVariables                #-}
 
 module Main where
 
 import qualified Language.C.Inline          as C
-import           Control.Monad                   (void)
-import           Control.Monad.IO.Class          (MonadIO, liftIO)
+import           Foreign.C.Types                 (CInt)
+import           Foreign.Ptr                     (Ptr)
+import           Foreign.C.String                (peekCString)
+import           Foreign.Storable                (Storable)
+import qualified Foreign.Marshal.Alloc      as Ptr
+
+import           Control.Monad.IO.Class          (liftIO)
 import           Control.Monad.Trans.Except      (ExceptT, runExceptT)
+import           Control.Monad.Trans.Reader      (ReaderT(..), ask)
 import           Control.Exception               (Exception, throw)
-import           Data.Text                       (Text)
+
+import           Data.Typeable                   (cast)
 import           Data.Data
+import           Data.IORef
+import           Data.Text                       (Text)
+import           Data.String                     (IsString(..))
+import qualified Data.List                  as L (partition)
 
-C.include "portaudio.h"
+default (Text)
 
-data PaConnectionStatus = PaInitialized | PaTerminated
+C.include "portaudio.h" -- stack ghci --ghci-options='-fobject-code -O0 -lportaudio'
+C.include "<string.h>"
 
-data PaConnection (connStatus :: PaConnectionStatus) = MkConnection
+type PointerConstraint a = (Storable a, Eq a, Typeable a)
+data AnyPointer = forall a. PointerConstraint a => AnyPointer (Ptr a)
 
-type PaMonad a = ExceptT PaErrorCode IO a
+instance Eq AnyPointer where
+  AnyPointer a == AnyPointer b = Just a == cast b
 
-initializePa :: PaMonad (PaConnection 'PaInitialized)
-initializePa = do liftIO [C.exp| int { Pa_Initialize() } |]
-                  return MkConnection
+data Pa = Pa
+ { heapPointers :: IORef [AnyPointer]
+ }
 
-terminatePa :: PaConnection 'PaInitialized -> PaMonad (PaConnection 'PaTerminated)
-terminatePa _ = do liftIO [C.exp| int { Pa_Terminate() } |]
-                   return MkConnection
+type PaMonad a   = ReaderT Pa (ExceptT PaErrorCode IO) a
+type PaError     = CInt
 
-terminatePa' :: PaConnection 'PaInitialized -> PaMonad ()
-terminatePa' = void . terminatePa
+pmAlloc :: forall p. PointerConstraint p => IO (Ptr p) -> PaMonad (Ptr p)
+pmAlloc funAlloc = do
+  ptr <- liftIO funAlloc :: PaMonad (Ptr p)
+  pa <- ask
+  liftIO $ modifyIORef' (heapPointers pa) (\hp -> AnyPointer ptr : hp)
+  return ptr
+
+pmFree :: forall p. PointerConstraint p => Ptr p -> PaMonad ()
+pmFree ptr = ask >>= \pa -> liftIO $ do
+  hp <- readIORef (heapPointers pa)
+  let (sameMemoryPointers, restPointers) = L.partition (\x -> x == AnyPointer ptr) hp
+  case sameMemoryPointers of
+    [] -> return ()
+    (AnyPointer _:_) -> do
+      Ptr.free ptr
+      modifyIORef' (heapPointers pa) (\_ -> restPointers)
+      return ()
+
+pmFreeAll :: PaMonad ()
+pmFreeAll = ask >>= \pa -> do
+  hp <- liftIO (readIORef . heapPointers $ pa)
+  mapM_ (\(AnyPointer ptr) -> pmFree ptr) hp
 
 data PaErrorCode =
     PaNotInitialized
@@ -73,35 +109,55 @@ codeToPaError code =
        then fromConstr (constrs !! index)
        else PaError
 
-type PaTime        = Double
+paMayThrow :: IO PaError -> PaMonad Int
+paMayThrow action = liftIO action >>= \x ->
+  let result = fromIntegral x
+  in if result < 0
+       then throw (codeToPaError result)
+       else return result
+
+runPaT :: PaMonad a -> IO (Either PaErrorCode a)
+runPaT action = do
+  hp <- newIORef mempty
+  let pa = Pa hp
+  runExceptT . flip runReaderT pa $ do
+    _ <- paMayThrow [C.exp| int { Pa_Initialize() } |]
+    result <- action
+    _ <- paMayThrow [C.exp| int { Pa_Terminate()  } |]
+    return result
 
 data PaDeviceInfo = PaDeviceInfo
-  { paDeviceName                     :: Text
-  , paDeviceMaxInputChannels         :: Int
-  , paDeviceMaxOutputChannels        :: Int
-  , paDeviceDefaultLowInputLatency   :: PaTime
-  , paDeviceDefaultLowOutputLatency  :: PaTime
-  , paDeviceDefaultHighInputLatency  :: PaTime
-  , paDeviceDefaultHighOutputLatency :: PaTime
-  , paDeviceDefaultSampleRate        :: Double
+  { paDeviceIndex :: Int
+  , paDeviceName  :: Text
   } deriving (Show)
 
-getDeviceCount :: PaConnection 'PaInitialized -> PaMonad Int
-getDeviceCount _ = do
-  numDevices <- liftIO $ fromIntegral <$> [C.exp| int { Pa_GetDeviceCount() } |]
-  if numDevices < 0
-    then throw (codeToPaError numDevices)
-    else return numDevices
+getDeviceCount :: PaMonad Int
+getDeviceCount = paMayThrow [C.exp| int { Pa_GetDeviceCount() } |]
 
-getDevices :: PaConnection 'PaInitialized -> Int -> PaMonad [PaDeviceInfo]
-getDevices _ 0 = return []
-getDevices _ devicesCount = undefined
+getDevices :: Int -> PaMonad [PaDeviceInfo]
+getDevices 0 = return []
+getDevices devicesCount =
+  let index = fromIntegral $ pred devicesCount
+  in do
+    ptrDeviceName <- pmAlloc Ptr.malloc
+
+    _ <- liftIO $ [C.block| int {
+      const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo( $(int index) );
+      strcpy( $(char* ptrDeviceName), deviceInfo->name);
+      return 0;
+    }|]
+
+    deviceName <- liftIO $ peekCString ptrDeviceName
+    pmFree ptrDeviceName
+    let deviceInfo = PaDeviceInfo (fromIntegral index) (fromString deviceName)
+
+    xs <- getDevices (devicesCount - 1)
+    return $ deviceInfo : xs
 
 main :: IO ()
 main = do
-  runExceptT $ do
-    conn <- initializePa
-    numDevices <- getDeviceCount conn
-    liftIO $ print numDevices
-    terminatePa' conn
+  _ <- runPaT $ do
+    numDevices <- getDeviceCount
+    devices <- getDevices numDevices
+    liftIO $ print devices
   return ()
